63a75a9e14030730f353ec13a71a0e18316438f4
Håkon Arneng Holmstedt <Hakon.Arneng.Holmstedt@nav.no>
2018-12-10 09:40:43 +0100

    cleaned up imports

--- src/test/java/no/nav/nare/core/evaluation/ResultTest.java
+++ src/test/java/no/nav/nare/core/evaluation/ResultTest.java
@@ -1,40 +1,41 @@
 package no.nav.nare.core.evaluation;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import static no.nav.nare.core.evaluation.Result.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class ResultTest {
 
     @Test
     public void allPermutations() {
         // and
-        Assertions.assertThat(Result.YES.and(Result.YES)).isEqualTo(Result.YES);
-        Assertions.assertThat(Result.YES.and(Result.NO)).isEqualTo(Result.NO);
-        Assertions.assertThat(Result.NO.and(Result.YES)).isEqualTo(Result.NO);
-        Assertions.assertThat(Result.NO.and(Result.NO)).isEqualTo(Result.NO);
+        assertThat(YES.and(YES)).isEqualTo(YES);
+        assertThat(YES.and(NO)).isEqualTo(NO);
+        assertThat(NO.and(YES)).isEqualTo(NO);
+        assertThat(NO.and(NO)).isEqualTo(NO);
 
-        Assertions.assertThat(Result.YES.and(Result.MAYBE)).isEqualTo(Result.MAYBE);
-        Assertions.assertThat(Result.NO.and(Result.MAYBE)).isEqualTo(Result.NO);
-        Assertions.assertThat(Result.MAYBE.and(Result.MAYBE)).isEqualTo(Result.MAYBE);
-        Assertions.assertThat(Result.MAYBE.and(Result.YES)).isEqualTo(Result.MAYBE);
-        Assertions.assertThat(Result.MAYBE.and(Result.NO)).isEqualTo(Result.NO);
+        assertThat(YES.and(MAYBE)).isEqualTo(MAYBE);
+        assertThat(NO.and(MAYBE)).isEqualTo(NO);
+        assertThat(MAYBE.and(MAYBE)).isEqualTo(MAYBE);
+        assertThat(MAYBE.and(YES)).isEqualTo(MAYBE);
+        assertThat(MAYBE.and(NO)).isEqualTo(NO);
 
         // or
-        Assertions.assertThat(Result.YES.or(Result.YES)).isEqualTo(Result.YES);
-        Assertions.assertThat(Result.YES.or(Result.NO)).isEqualTo(Result.YES);
-        Assertions.assertThat(Result.NO.or(Result.YES)).isEqualTo(Result.YES);
-        Assertions.assertThat(Result.NO.or(Result.NO)).isEqualTo(Result.NO);
+        assertThat(YES.or(YES)).isEqualTo(YES);
+        assertThat(YES.or(NO)).isEqualTo(YES);
+        assertThat(NO.or(YES)).isEqualTo(YES);
+        assertThat(NO.or(NO)).isEqualTo(NO);
 
-        Assertions.assertThat(Result.YES.or(Result.MAYBE)).isEqualTo(Result.YES);
-        Assertions.assertThat(Result.NO.or(Result.MAYBE)).isEqualTo(Result.MAYBE);
-        Assertions.assertThat(Result.MAYBE.or(Result.MAYBE)).isEqualTo(Result.MAYBE);
-        Assertions.assertThat(Result.MAYBE.or(Result.YES)).isEqualTo(Result.YES);
-        Assertions.assertThat(Result.MAYBE.or(Result.NO)).isEqualTo(Result.MAYBE);
+        assertThat(YES.or(MAYBE)).isEqualTo(YES);
+        assertThat(NO.or(MAYBE)).isEqualTo(MAYBE);
+        assertThat(MAYBE.or(MAYBE)).isEqualTo(MAYBE);
+        assertThat(MAYBE.or(YES)).isEqualTo(YES);
+        assertThat(MAYBE.or(NO)).isEqualTo(MAYBE);
 
         // not
-        Assertions.assertThat(Result.YES.not()).isEqualTo(Result.NO);
-        Assertions.assertThat(Result.NO.not()).isEqualTo(Result.YES);
-
-        Assertions.assertThat(Result.MAYBE.not()).isEqualTo(Result.MAYBE);
+        assertThat(YES.not()).isEqualTo(NO);
+        assertThat(NO.not()).isEqualTo(YES);
+        assertThat(MAYBE.not()).isEqualTo(MAYBE);
     }
 }
