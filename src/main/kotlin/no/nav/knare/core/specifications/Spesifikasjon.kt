de125d60fa10086066bcc57f8d43c25d9be765db
Håkon Arneng Holmstedt <Hakon.Arneng.Holmstedt@nav.no>
2018-12-17 20:57:08 +0100

    call the functions, don't just point to them.

--- src/main/kotlin/no/nav/knare/core/specifications/Spesifikasjon.kt
+++ src/main/kotlin/no/nav/knare/core/specifications/Spesifikasjon.kt
@@ -40,8 +40,8 @@ class OgSpesifikasjon<T>(
         val left: Spesifikasjon<T>,
         val right: Spesifikasjon<T>
 ) : Spesifikasjon<T>() {
-    override var beskrivelse: () -> String = { "(${left.beskrivelse} OG ${right.beskrivelse})" }
-    override var identitet: () -> String = { "(${left.identitet}) OG ${right.identitet}" }
+    override var beskrivelse: () -> String = { "(${left.beskrivelse()} OG ${right.beskrivelse()})" }
+    override var identitet: () -> String = { "(${left.identitet()}) OG ${right.identitet()}" }
     override fun evaluer(t: T): Evaluering {
         return evaluering(beskrivelse(), identitet(), og(left.evaluer(t), right.evaluer(t)))
     }
@@ -51,8 +51,8 @@ class EllerSpesifikasjon<T>(
         val left: Spesifikasjon<T>,
         val right: Spesifikasjon<T>
 ) : Spesifikasjon<T>() {
-    override var beskrivelse: () -> String = { "(${left.beskrivelse} ELLER ${right.beskrivelse})" }
-    override var identitet: () -> String = { "(${left.identitet}) ELLER ${right.identitet}" }
+    override var beskrivelse: () -> String = { "(${left.beskrivelse()} ELLER ${right.beskrivelse()})" }
+    override var identitet: () -> String = { "(${left.identitet()}) ELLER ${right.identitet()}" }
     override fun evaluer(t: T): Evaluering {
         return evaluering(beskrivelse(), identitet(), eller(left.evaluer(t), right.evaluer(t)))
     }
@@ -61,8 +61,8 @@ class EllerSpesifikasjon<T>(
 class IkkeSpesifikasjon<T>(
         val spec: Spesifikasjon<T>
 ) : Spesifikasjon<T>() {
-    override var beskrivelse: () -> String = { "(IKKE ${spec.beskrivelse})" }
-    override var identitet: () -> String = { "(IKKE ${spec.identitet})" }
+    override var beskrivelse: () -> String = { "(IKKE ${spec.beskrivelse()})" }
+    override var identitet: () -> String = { "(IKKE ${spec.identitet()})" }
     override fun evaluer(t: T): Evaluering {
         return evaluering(beskrivelse(), identitet(), ikke(spec.evaluer(t)))
     }
