a8768aca036063ea4122d26951343b178de4bfc1
Håkon Arneng Holmstedt <Hakon.Arneng.Holmstedt@nav.no>
2018-12-17 20:44:08 +0100

    got children working.

--- src/main/kotlin/no/nav/knare/core/example/Regelsett.kt
+++ src/main/kotlin/no/nav/knare/core/example/Regelsett.kt
@@ -4,90 +4,112 @@ import no.nav.knare.core.evaluations.Evaluation
 import no.nav.knare.core.evaluations.Evaluation.Companion.ja
 import no.nav.knare.core.evaluations.Evaluation.Companion.nei
 import no.nav.knare.core.specifications.Specification
+import no.nav.knare.core.specifications.Specification.Companion.not
+import no.nav.knare.core.specifications.Specification.Companion.rule
 
 class HarArbeidetSisteMnd(
-        val month: Int,
-        override val description: String = "Har dokumentert sammenhengende arbeid siste $month mnd",
-        override val identity: String = "FK_VK_10.x"
-) : Specification<Soknad>({
-    if (it.hovedsoker.mndArbeid >= month) Evaluation.ja("Person har jobbet ${it.hovedsoker.mndArbeid} måneder, som er tilstrekkelig", description, identity)
-    else Evaluation.nei("Person er oppfort med ${it.hovedsoker.mndArbeid} mnd arbeid. Dekker ikke kravet til ${month} mnd med arbeid", description, identity)
-})
+        val month: Int
+) : Specification<Soknad>() {
+    override var description: () -> String = { "Har dokumentert sammenhengende arbeid siste $month mnd" }
+    override var identity: () -> String = { "FK_VK_10.x" }
+    override fun evaluate(søknad: Soknad): Evaluation {
+        return if (søknad.hovedsoker.mndArbeid >= month) Evaluation.ja("Person har jobbet ${søknad.hovedsoker.mndArbeid} måneder, som er tilstrekkelig", description(), identity())
+        else Evaluation.nei("Person er oppfort med ${søknad.hovedsoker.mndArbeid} mnd arbeid. Dekker ikke kravet til ${month} mnd med arbeid", description(), identity())
+    }
+}
 
 class HarRettTilForeldrePenger(
-        rolle: Rolle,
-        override val description: String = "Har søker med rolle $rolle rett til foreldrepenger?",
-        override val identity: String = "FK_VK_10.1"
-) : Specification<Soknad>({
-    val sokerIRolle = it.hentSøkerIRolle(rolle)
-    when {
-        sokerIRolle == null -> nei("Ingen søker med rolle $rolle", description, identity)
-        !sokerIRolle.rettTilFp -> nei("Søker med rolle $rolle har ikke rett til foreldrepenger", description, identity)
-        else -> ja("Søker med rolle $rolle har rett til foreldrepenger", description, identity)
+        val rolle: Rolle
+
+) : Specification<Soknad>() {
+    override var description: () -> String = { "Har søker med rolle $rolle rett til foreldrepenger?" }
+    override var identity: () -> String = { "FK_VK_10.1" }
+    override fun evaluate(søknad: Soknad): Evaluation {
+        val sokerIRolle = søknad.hentSøkerIRolle(rolle)
+        return when {
+            sokerIRolle == null -> nei("Ingen søker med rolle $rolle", description(), identity())
+            !sokerIRolle.rettTilFp -> nei("Søker med rolle $rolle har ikke rett til foreldrepenger", description(), identity())
+            else -> ja("Søker med rolle $rolle har rett til foreldrepenger", description(), identity())
+        }
     }
-})
+}
 
 class HarUttaksplanForModreKvote(
-        soknadstype: Soknadstype,
-        uttaksplan: Uttaksplan,
-        override val description: String = "",
-        override val identity: String = "FK_VK 10.4/FK_VK 10.5/FK_VK 10.6"
-) : Specification<Soknad>({
-    val mor = it.hentSøkerIRolle(Rolle.MOR)
-    when {
-        mor == null -> nei("Ingen søker med rolle ${Rolle.MOR}", description, identity)
-        mor.uttaksplan == null -> nei("Det foreligger ingen uttaksplan for ${Rolle.MOR}", description, identity)
-        uttaksplan == mor.uttaksplan -> ja("Mødrekvote tas ${uttaksplan.description}", description, identity)
-        else -> nei("Mødrekvote tas ikke ${uttaksplan.description} $soknadstype", description, identity)
+        val soknadstype: Soknadstype,
+        val uttaksplan: Uttaksplan
+) : Specification<Soknad>() {
+    override var description: () -> String = { "" }
+    override var identity: () -> String = { "FK_VK 10.4/FK_VK 10.5/FK_VK 10.6" }
+    override fun evaluate(søknad: Soknad): Evaluation {
+        val mor = søknad.hentSøkerIRolle(Rolle.MOR)
+        return when {
+            mor == null -> nei("Ingen søker med rolle ${Rolle.MOR}", description(), identity())
+            mor.uttaksplan == null -> nei("Det foreligger ingen uttaksplan for ${Rolle.MOR}", description(), identity())
+            uttaksplan == mor.uttaksplan -> ja("Mødrekvote tas ${uttaksplan.description}", description(), identity())
+            else -> nei("Mødrekvote tas ikke ${uttaksplan.description} $soknadstype", description(), identity())
+        }
     }
-})
+}
 
 class SoknadGjelder(
-        soknadstype: Soknadstype,
-        override val description: String = "Soknad gjelder $soknadstype",
-        override val identity: String = "SoknadGjelder"
-) : Specification<Soknad>({
-    when (soknadstype) {
-        it.soknadstype -> ja("Søknad gjelder $soknadstype", description, identity)
-        else -> nei("Søknad gjelder ikke $soknadstype", description, identity)
+        val soknadstype: Soknadstype
+) : Specification<Soknad>() {
+    override var description: () -> String = { "Soknad gjelder $soknadstype" }
+    override var identity: () -> String = { "SoknadGjelder" }
+    override fun evaluate(søknad: Soknad): Evaluation {
+        return when (soknadstype) {
+            søknad.soknadstype -> ja("Søknad gjelder $soknadstype", description(), identity())
+            else -> nei("Søknad gjelder ikke $soknadstype", description(), identity())
+        }
     }
-})
+}
 
 class Regelsett {
 
-    private val harBeggeForeldreRettTilForeldrePenger = HarRettTilForeldrePenger(Rolle.MOR).and(
-            right = HarRettTilForeldrePenger(Rolle.FAR),
+    private val harBeggeForeldreRettTilForeldrePenger = rule(
+            spec = HarRettTilForeldrePenger(Rolle.MOR).and(HarRettTilForeldrePenger(Rolle.FAR)),
             identity = "FK_VK_10.1",
-            description = "Har begge foreldre rett til foreldrepenger?"
-    )
+            description = "Har begge foreldre rett til foreldrepenger?")
 
-    private val gjelderSoknadFødsel = SoknadGjelder(
-            soknadstype = Soknadstype.FODSEL,
+    private val gjelderSoknadFødsel = rule(
+            spec = SoknadGjelder(Soknadstype.FODSEL),
             identity = "FK_VK 10.2",
             description = "Gjelder søknad fødsel?"
     )
-    private val gjelderSoknadAdopsjon = SoknadGjelder(
-            soknadstype = Soknadstype.ADOPSJON,
+    private val gjelderSoknadAdopsjon = rule(
+            spec = SoknadGjelder(Soknadstype.ADOPSJON),
             identity = "FK_VK 10.3",
             description = "Gjelder søknad adopsjon?"
     )
-    private val harUttaksplanEtterFodsel = HarUttaksplanForModreKvote(
-            soknadstype = Soknadstype.FODSEL,
-            uttaksplan = Uttaksplan.SAMMENHENGENDE,
+    private val harUttaksplanEtterFodsel = rule(
+            spec = HarUttaksplanForModreKvote(
+                    soknadstype = Soknadstype.FODSEL,
+                    uttaksplan = Uttaksplan.SAMMENHENGENDE),
             identity = "FK_VK_10.4",
             description = "Har mor uttaksplan sammenhengende or tre år etter fødsel?"
     )
-    private val harUttaksplanEtterAdopsjon = HarUttaksplanForModreKvote(
-            soknadstype = Soknadstype.ADOPSJON,
-            uttaksplan = Uttaksplan.INNEN_3_AAR,
+    private val harUttaksplanEtterAdopsjon = rule(
+            spec = HarUttaksplanForModreKvote(
+                    soknadstype = Soknadstype.ADOPSJON,
+                    uttaksplan = Uttaksplan.INNEN_3_AAR),
             identity = "FK_VK_10.5",
             description = "Har mor uttaksplan sammenhengende or tre år etter adopsjon?"
     )
-    private val vilkårForFødsel = harBeggeForeldreRettTilForeldrePenger.and(gjelderSoknadFødsel).and(harUttaksplanEtterFodsel, identity = "FK_VK.10.A")
-    private val vilkårForAdopsjon = harBeggeForeldreRettTilForeldrePenger
-            .and(gjelderSoknadFødsel.not(description = "søknad gjelder ikke fødsel"))
-            .and(gjelderSoknadAdopsjon)
-            .and(harUttaksplanEtterAdopsjon, identity = "FK_VK.10.B")
+    private val vilkårForFødsel = rule(
+            spec = harBeggeForeldreRettTilForeldrePenger.and(gjelderSoknadFødsel).and(harUttaksplanEtterFodsel),
+            identity = "FK_VK.10.A",
+            description = ""
+    )
+    private val vilkårForAdopsjon = rule(
+            spec = harBeggeForeldreRettTilForeldrePenger
+                    .and(rule(spec = not(gjelderSoknadFødsel), description = "søknad gjelder ikke fødsel", identity = ""))
+                    .and(gjelderSoknadAdopsjon)
+                    .and(harUttaksplanEtterAdopsjon),
+            identity = "FK_VK.10.B",
+            description = "")
 
-    val mødreKvote = vilkårForAdopsjon.or(vilkårForFødsel, identity = "FK_VK.10", description = "Er vilkår for mødrekvote oppfylt for enten fødsel or adopsjon?")
+    val mødreKvote = rule(
+            spec = vilkårForAdopsjon.or(vilkårForFødsel),
+            identity = "FK_VK.10",
+            description = "Er vilkår for mødrekvote oppfylt for enten fødsel or adopsjon?")
 }
--- src/main/kotlin/no/nav/knare/core/specifications/Specification.kt
+++ src/main/kotlin/no/nav/knare/core/specifications/Specification.kt
@@ -1,40 +1,69 @@
 package no.nav.knare.core.specifications
 
 import no.nav.knare.core.evaluations.Evaluation
+import no.nav.knare.core.evaluations.Evaluation.Companion.and
+import no.nav.knare.core.evaluations.Evaluation.Companion.evaluation
+import no.nav.knare.core.evaluations.Evaluation.Companion.not
+import no.nav.knare.core.evaluations.Evaluation.Companion.or
 
-abstract class Specification<T>(val evaluate: (T) -> Evaluation) {
-    abstract val description: String
-    abstract val identity: String
+abstract class Specification<T> {
+    open var description: () -> String = { "default description" }
+    open var identity: () -> String = { "default identity" }
+    abstract fun evaluate(t: T): Evaluation
 
-    fun and(right: Specification<T>, description: String = "", identity: String = ""): Specification<T> {
-        return AndSpecification(left = this, right = right, description = description, identity = identity)
+    fun and(right: Specification<T>): Specification<T> {
+        return AndSpecification(left = this, right = right)
     }
 
-    fun or(right: Specification<T>, description: String = "", identity: String = ""): Specification<T> {
-        return OrSpecification(left = this, right = right, description =  description, identity = identity)
+    fun or(right: Specification<T>): Specification<T> {
+        return OrSpecification(left = this, right = right)
     }
 
-    fun not(description: String = "", identity: String = ""): Specification<T> {
-        return NotSpecification(spec = this, description = description, identity = identity)
+    fun not(): Specification<T> {
+        return NotSpecification(spec = this)
+    }
+
+    companion object {
+        fun <T> rule(spec: Specification<T>, description: String, identity: String): Specification<T> {
+            spec.description = fun(): String = description
+            spec.identity = fun(): String = identity
+            return spec
+        }
+
+        fun <T> not(spec: Specification<T>): Specification<T> {
+            return NotSpecification(spec)
+        }
     }
 }
 
 class AndSpecification<T>(
-        left: Specification<T>,
-        right: Specification<T>,
-        override val description: String = "(${left.description} AND ${right.description})",
-        override val identity: String = "(${left.identity}) AND ${right.identity}"
-) : Specification<T>({ left.evaluate(it).and(right.evaluate(it)) })
+        val left: Specification<T>,
+        val right: Specification<T>
+) : Specification<T>() {
+    override var description: () -> String = { "(${left.description} AND ${right.description})" }
+    override var identity: () -> String = { "(${left.identity}) AND ${right.identity}" }
+    override fun evaluate(t: T): Evaluation {
+        return evaluation(description(), identity(), and(left.evaluate(t), right.evaluate(t)))
+    }
+}
 
 class OrSpecification<T>(
-        left: Specification<T>,
-        right: Specification<T>,
-        override val description: String = "(${left.description} OR ${right.description})",
-        override val identity: String = "(${left.identity}) OR ${right.identity}"
-) : Specification<T>({ left.evaluate(it).or(right.evaluate(it)) })
+        val left: Specification<T>,
+        val right: Specification<T>
+) : Specification<T>() {
+    override var description: () -> String = { "(${left.description} OR ${right.description})" }
+    override var identity: () -> String = { "(${left.identity}) OR ${right.identity}" }
+    override fun evaluate(t: T): Evaluation {
+        return evaluation(description(), identity(), or(left.evaluate(t), right.evaluate(t)))
+    }
+}
 
 class NotSpecification<T>(
-        spec: Specification<T>,
-        override val description: String = "(NOT ${spec.description})",
-        override val identity: String = "(NOT ${spec.identity})"
-) : Specification<T>({ spec.evaluate(it).not() })
+        val spec: Specification<T>
+) : Specification<T>() {
+    override var description: () -> String = { "(NOT ${spec.description})" }
+    override var identity: () -> String = { "(NOT ${spec.identity})" }
+    override fun evaluate(t: T): Evaluation {
+        return evaluation(description(), identity(), not(spec.evaluate(t)))
+    }
+}
