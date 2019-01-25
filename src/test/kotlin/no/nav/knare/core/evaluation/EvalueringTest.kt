c8c27d36c46e163197cc92971cdebbb5c961a71c
Truls Jørgensen <trulsjor@gmail.com>
2019-01-25 15:25:25 +0100

    Added identity and description propagation from Spec to Eval. Also, d3 visualization with example Mødrekvote

--- src/test/kotlin/no/nav/knare/core/evaluation/EvalueringTest.kt
+++ src/test/kotlin/no/nav/knare/core/evaluation/EvalueringTest.kt
@@ -1,32 +1,38 @@
-import no.nav.knare.core.evaluations.*
+import no.nav.knare.core.evaluations.Evaluering
 import no.nav.knare.core.evaluations.Evaluering.Companion.ja
 import no.nav.knare.core.evaluations.Evaluering.Companion.nei
-import no.nav.knare.core.evaluations.Resultat.*
-import org.junit.jupiter.api.*
-import org.junit.jupiter.api.Assertions.*
+import no.nav.knare.core.evaluations.Operator
+import no.nav.knare.core.evaluations.Resultat.JA
+import no.nav.knare.core.evaluations.Resultat.NEI
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class EvalueringTest {
 
     @Test
     fun eller() {
-        val left = ja("left reason")
-        val right = nei("right reason")
-        val expected = Evaluering(JA, "left reason ELLER right reason", Operatør.ELLER, listOf(left, right))
+        val left = ja("left begrunnelse")
+        val right = nei("right begrunnelse")
+        val expected = Evaluering(
+                resultat = JA,
+                begrunnelse = "(left begrunnelse ELLER right begrunnelse)",
+                operator = Operator.ELLER,
+                children = listOf(left, right))
         assertEquals(expected, left eller right)
     }
 
     @Test
     fun og() {
-        val left = ja("left reason")
-        val right = nei("right reason")
-        val expected = Evaluering(NEI, "left reason OG right reason", Operatør.OG, listOf(left, right))
+        val left = ja("left begrunnelse")
+        val right = nei("right begrunnelse")
+        val expected = Evaluering(resultat = NEI, begrunnelse = "(left begrunnelse OG right begrunnelse)", operator = Operator.OG, children = listOf(left, right))
         assertEquals(expected, left og right)
     }
 
     @Test
     fun ikke() {
-        val orig = ja("orig reason")
-        val expected = Evaluering(NEI, "IKKE orig reason", Operatør.IKKE, listOf(orig))
+        val orig = ja("orig begrunnelse")
+        val expected = Evaluering(resultat = NEI, begrunnelse = "(IKKE orig begrunnelse)", operator = Operator.IKKE, children = listOf(orig))
         assertEquals(expected, orig.ikke())
     }
 }
---
+++ src/test/kotlin/no/nav/knare/demo/Demo.kt
@@ -0,0 +1,22 @@
+package no.nav.knare.demo
+
+import com.google.gson.Gson
+import no.nav.knare.core.demo.*
+import spark.Request
+import spark.Response
+import spark.Spark.*
+
+
+fun main(args: Array<String>) {
+    val gson = Gson()
+    port(1339)
+    staticFiles.location("/public")
+    get("/api", { _: Request, _: Response -> Regelsett().mødrekvote.evaluer(søknad) }, { gson.toJson(it) })
+
+}
+
+private val søknad = Soknad(
+        hovedsoker = Person(name = "Mor", rolle = Rolle.MOR, address = "Oslo", inntekt = 600000, mndArbeid = 24, rettTilFp = true, yrke = "A", uttaksplan = Uttaksplan.SAMMENHENGENDE),
+        medsoker = Person(name = "Far", rolle = Rolle.FAR, address = "Oslo", inntekt = 500000, mndArbeid = 80, rettTilFp = true, yrke = "B", uttaksplan = null),
+        søknadstype = Soknadstype.FODSEL)
+
