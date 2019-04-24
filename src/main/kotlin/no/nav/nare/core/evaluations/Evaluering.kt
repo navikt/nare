038089738f0ac9b17fb6125eaea3a70ce0217e72
Truls Jørgensen <trulsjor@gmail.com>
2019-04-24 20:59:32 +0200

    Trying to move nare to n-tree

--- src/main/kotlin/no/nav/nare/core/evaluations/Evaluering.kt
+++ src/main/kotlin/no/nav/nare/core/evaluations/Evaluering.kt
@@ -12,14 +12,14 @@ data class Evaluering(
       resultat = resultat og other.resultat,
       begrunnelse = "($begrunnelse OG ${other.begrunnelse})",
       operator = Operator.OG,
-      children = listOf(this, other)
+      children = addChildren(this, other)
    )
 
    infix fun eller(other: Evaluering) = Evaluering(
       resultat = resultat eller other.resultat,
       begrunnelse = "($begrunnelse ELLER ${other.begrunnelse})",
       operator = Operator.ELLER,
-      children = listOf(this, other)
+      children = addChildren(this, other)
    )
 
    fun ikke() = Evaluering(
@@ -28,6 +28,12 @@ data class Evaluering(
       operator = Operator.IKKE,
       children = listOf(this)
    )
+
+   fun addChildren(s: Evaluering, o: Evaluering): List<Evaluering> {
+      if (o.identifikator.isBlank() && o.children.isNotEmpty()) return o.children + s
+      if (s.identifikator.isBlank() && s.children.isNotEmpty()) return s.children + o
+      return listOf(s, o)
+   }
 
    companion object {
       fun ja(begrunnelse: String) = Evaluering(Resultat.Ja, begrunnelse)
